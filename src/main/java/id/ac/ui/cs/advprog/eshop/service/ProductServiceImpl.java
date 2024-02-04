5dd9c885863afc60aef1b63a56321fa2bac122d3
Fredo Melvern Tanzil <fredotanzil@gmail.com>
2024-02-04 16:38:35 +0700

    Change edit & delete into productRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -34,28 +34,12 @@ public class ProductServiceImpl implements  ProductService{
 
     @Override
     public Product findById(String id) {
-        Iterator<Product> productIterator = productRepository.findAll();
-
-        while (productIterator.hasNext()){
-            Product product = productIterator.next();
-            if (product.getProductId().equals(id)){
-                return product;
-            }
-        }
-        throw new IllegalArgumentException("Product with id " + id + " not found");
+        productRepository.findById(id);
     }
 
     @Override
     public void deleteProductById(String id){
-        Iterator<Product> productIterator = productRepository.findAll();
-
-        while (productIterator.hasNext()){
-            Product product = productIterator.next();
-            if (product.getProductId().equals(id)){
-                productIterator.remove();
-                break;
-            }
-        }
+        productRepository.deleteProductById(id);
     }
 
 }
